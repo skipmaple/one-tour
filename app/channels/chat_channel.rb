24fdf70074fe9ce65c4cd51f4e5249e7894a8592
drewlee <skipmaple@gmail.com>
2026-04-15 21:13:37 +0800

    channel: ChatChannel tour_id + perms

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,16 +1,10 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    guidebook = Guidebook.find(params[:guidebook_id])
-
-    if guidebook.editable_by?(current_user)
-      stream_from chat_stream_name(params[:guidebook_id])
+    tour = Tour.find_by(id: params[:tour_id])
+    if tour&.visible_to?(current_user)
+      stream_from "chat_tour_#{tour.id}_user_#{current_user.id}"
     else
       reject
     end
   end
-
-  private
-    def chat_stream_name(guidebook_id)
-      "chat_guidebook_#{guidebook_id}_user_#{current_user.id}"
-    end
 end
